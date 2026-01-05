a7d0d3d5869039207bd039ddece2bfddaa77bd7e
shahabaz Madihalli <shahabazmadihalli@gmail.com>
2026-01-05 17:32:03 +0530

    first commit sub branches

---
+++ 03_Northwind CLI/webapp/Component.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/model/BindingMode'
+], function (UIComponent, JSONModel, BindingMode) {
+    'use strict';
+
+    return UIComponent.extend("ui5.demo.apps.Component", {
+
+        metadata: {
+            "interfaces": ["sap.ui.core.IAsyncContentCreation"],
+            "manifest": "json"
+
+
+        },
+
+        init: function () {
+            UIComponent.prototype.init.apply(this, arguments);
+
+            var i18n = this.getModel("i18n");
+            var oBundle = i18n.getResourceBundle(),
+                mData = new JSONModel({
+                    message: oBundle.getText("msg")
+                });
+            mData.setDefaultBindingMode(BindingMode.TwoWay);
+            //this.setModel(mData);
+        }
+
+    });
+
+});
